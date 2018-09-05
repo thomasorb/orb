added high order phase computation capabilities. old phase files removed. C1 phase file computed. the others must be recomputed also. minor bugs corrected.

orb/core.py
@@ -704,7 +704,7 @@ class Tools(object):
         :param filter_name: Name of the filter.
         """
         phase_file_path =  self._get_orb_data_file_path(
-            "phase_" + filter_name + ".orb")
+            "phase_" + filter_name + ".hdf5")
         
         if not os.path.exists(phase_file_path):
              warnings.warn(
@@ -5462,17 +5462,29 @@ class Vector1d(object):
     def __init__(self, vector, params=None, **kwargs):
         """Init method.
 
-        :param vector: A 1d numpy.ndarray vector.
+        :param vector: A 1d numpy.ndarray vector or a path to an hdf5
+          vector file.
 
         :param params: (Optional) A dict containing additional
           parameters giving access to more methods. The needed params
-          are stored in self.needed_params (default None).
+          are stored in self.needed_params (default None). If vector
+          is a path it must be set to None.
 
         :param kwargs: (Optional) Keyword arguments, can be used to
           supply observation parameters not included in the params
           dict. These parameters take precedence over the parameters
-          supplied in the params dictionnary.    
+          supplied in the params dictionnary.
+
         """
+        if isinstance(vector, str):
+            if params is not None:
+                raise TypeError('params must be set to None if vector is a path')
+            with utils.io.open_hdf5(vector, 'r') as hdffile:
+                params = dict()
+                for iparam in hdffile.attrs:
+                    params[iparam] = hdffile.attrs[iparam]
+                vector = hdffile['/vector'][:]
+
         if isinstance(vector, self.__class__):
             vector = np.copy(vector.data)
         else: vector = np.copy(vector)
@@ -5511,6 +5523,7 @@ class Vector1d(object):
             if not isinstance(params, dict):
                 raise TypeError('params must be a dict ! not a {}'.format(type(params)))
             self.params = ROParams()
+                
             for iparam in self.needed_params:
                 try:
                     self.params[iparam] = kwargs[iparam]
@@ -5549,6 +5562,20 @@ class Vector1d(object):
     def copy(self):
         """Return a copy of the instance"""
         return self.__class__(np.copy(self.data), params=self.params.convert())
+
+    def writeto(self, path):
+        """Write vector and params to an hdf file
+
+        :param path: hdf file path.
+        """
+        with utils.io.open_hdf5(path, 'w') as hdffile:
+            for iparam in self.params:
+                hdffile.attrs[iparam] = self.params[iparam]
+
+            hdffile.create_dataset(
+                '/vector',
+                data=self.data)
+
 
 #################################################
 #### CLASS Axis #################################

orb/fft.py
@@ -239,7 +239,13 @@ class Cm1Vector1d(core.Vector1d):
     def __init__(self, spectrum, axis, params=None, **kwargs):
         """Init method.
 
-        :param vector: A 1d numpy.ndarray vector.
+        :param spectrum: A 1d numpy.ndarray vector or a path to an
+          hdf5 cm1 vector file (note that axis must be set to None in
+          this case).
+
+        :param axis: A 1d numpy.ndarray axis (if a file is loaded,
+          i.e. spectrum is a path to an hdf5 file, it must be set to
+          None).
         
         :param params: (Optional) A dict containing observation
           parameters (default None).
@@ -247,9 +253,15 @@ class Cm1Vector1d(core.Vector1d):
         :param kwargs: (Optional) Keyword arguments, can be used to
           supply observation parameters not included in the params
           dict. These parameters take precedence over the parameters
-          supplied in the params dictionnary.    
+          supplied in the params dictionnary.
+
         """
         core.Vector1d.__init__(self, spectrum, params=params, **kwargs)
+        if isinstance(spectrum, str):
+            if axis is not None:
+                raise TypeError('if spectrum is a path to an hdf5 file, axis must be set to None')
+            with utils.io.open_hdf5(spectrum, 'r') as hdffile:
+                axis = hdffile['/axis'][:]
 
         self.axis = core.Axis(axis)
 
@@ -290,10 +302,52 @@ class Cm1Vector1d(core.Vector1d):
         :param new_axis: Axis. Must be an orb.core.Axis instance.
         """
         if not isinstance(new_axis, core.Axis):
-            raise NotImplementedError('axis must be an orb.core.Axis instance')
-        f = scipy.interpolate.interp1d(self.axis.data, self.data, bounds_error=False)
+            raise TypeError('axis must be an orb.core.Axis instance')
+        f = scipy.interpolate.interp1d(self.axis.data.astype(np.float128),
+                                       self.data.astype(np.float128),
+                                       bounds_error=False)
         return self.__class__(
             f(new_axis.data), new_axis.data, params=self.params)
+
+    def add(self, cm1vector):
+        """Add another cm1 vector. Note that, if the axis differs, only the
+        common part is kept.
+
+        :param cm1vector: Must be a Cm1Vector instance.
+        """
+        if not isinstance(cm1vector, self.__class__):
+            raise TypeError('phase must be a {} instance'.format(self.__class__))
+        self.data += cm1vector.project(self.axis).data
+
+    def subtract(self, cm1vector):
+        """Subtract another cm1 vector. Note that, if the axis differs, only the
+        common part is kept.
+
+        :param cm1vector: Must be a Cm1Vector instance.
+        """
+        if not isinstance(cm1vector, self.__class__):
+            raise TypeError('phase must be a {} instance'.format(self.__class__))
+        self.data -= cm1vector.project(self.axis).data
+
+    
+    def writeto(self, path):
+        """Write cm1 vector and params to an hdf file
+
+        :param path: hdf file path.
+        """
+        with utils.io.open_hdf5(path, 'w') as hdffile:
+            for iparam in self.params:
+                hdffile.attrs[iparam] = self.params[iparam]
+
+            hdffile.create_dataset(
+                '/vector',
+                data=self.data)
+
+            hdffile.create_dataset(
+                '/axis',
+                data=self.axis.data)
+
+
     
 class Phase(Cm1Vector1d):
     """Phase class