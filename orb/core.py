bugfix related to Data params, computation of modeling error in order 0 phase map now based on a cleaned distribution (with sigmacut) to avoid spurious pixels indicating a modeling problem

orb/core.py
@@ -1890,6 +1890,11 @@ class Data(object):
         self.params.update(kwargs)
 
         # check params
+
+        if self.has_param('filter_file_path'):
+            if not self.has_param('filter_name'):
+                self.set_param('filter_name', self.params['filter_file_path'])
+        
         for iparam in self.needed_params:
             if iparam not in self.params:
                 raise StandardError('param {} must be set'.format(iparam))
@@ -2485,7 +2490,8 @@ class Cm1Vector1d(Vector1d):
                 else:
                     if np.any(check_axis != self.axis.data):
                         warnings.warn('provided axis is inconsistent with the given parameters')
-            else: raise StandardError('{} must all be provided'.format(self.obs_params))
+            #else:
+                #raise StandardError('{} must all be provided'.format(self.obs_params))
             
         if self.axis is None: raise StandardError('an axis must be provided or the observation parameters ({}) must be provided'.format(self.obs_params))
             

orb/cube.py
@@ -1068,7 +1068,14 @@ class Cube(HDFCube):
                     
     def get_base_axis(self):
         """Return the spectral axis (in cm-1) at the center of the cube"""
-        return core.Axis(self.params.base_axis)
+        if not self.has_param('base_axis'):
+            base_axis = utils.spectrum.create_cm1_axis(
+                self.dimz, self.params.step, self.params.order,
+                corr=self.get_axis_corr())
+        else:
+            base_axis = self.params.base_axis
+            
+        return core.Axis(base_axis)
 
     def get_uncalibrated_filter_bandpass(self):
         """Return filter bandpass as two 2d matrices (min, max) in pixels"""
@@ -2112,9 +2119,7 @@ class SpectralCube(Cube):
             self.params['hour_ut'] = np.array([0, 0, 0], dtype=float)
 
         # create base axis of the data
-        self.set_param('base_axis', utils.spectrum.create_cm1_axis(
-            self.dimz, self.params.step, self.params.order,
-            corr=self.params.axis_corr))
+        self.set_param('base_axis', self.get_base_axis())
 
         self.set_param('axis_min', np.min(self.params.base_axis))
         self.set_param('axis_max', np.max(self.params.base_axis))

orb/utils/image.py
@@ -653,16 +653,10 @@ def fit_map_theta(data_map, err_map, theta_map):
     w = 1. / np.array(sdevs)
     w /= np.nanmax(w)
     model = interpolate.UnivariateSpline(okthetas, means, w=w, ext=0, k=3, s=None)
-    model_err = interpolate.UnivariateSpline(okthetas, sdevs, w=w, ext=0, k=3, s=None)
-
-    # import pylab as pl
-    # pl.plot(okthetas, means)
-    # pl.plot(okthetas, model(okthetas))
-    # #pl.plot(okthetas, means)
-    # pl.show()
-    
+    model_err = interpolate.UnivariateSpline(okthetas, sdevs, w=w, ext=0, k=3, s=None)   
 
     err = (model(theta_map) - data_map)[np.nonzero(pixmap)]
+    err = orb.utils.stats.sigmacut(err, sigma=2.5)
     logging.info('modeling error: {} (uncertainty on data: {})'.format(
         np.nanstd(err),
         np.nanmedian(sdevs)))