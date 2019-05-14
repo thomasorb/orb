72f124f4d1f726aa1d2a0e97cc2a0a8a5e26be7d
Thomas Martin <thomas.martin.1@ulaval.ca>
2019-05-14 15:15:21 -0400

    bugfix in setup.py reference to README.rst changed to README.md

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ packages = find_packages(where=".")
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
     
 extensions = [
