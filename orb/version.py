a99cd3b873770c25820570ce09fc822036e2db96
Thomas Martin <thomas.martin.1@ulaval.ca>
2019-12-16 10:55:00 -0500

    version.py bug when git is not installed corrected

--- orb/version.py
+++ orb/version.py
@@ -1,16 +1,18 @@
 import os
+import logging
 
 __version__ = 'unknown'
 
 try: import git
-except ImportError: pass
-
-# get git branch (if possible)
-repo_path = os.path.abspath(os.path.dirname(__file__) + os.sep + '..')
-try:
-    repo = git.Repo(repo_path)
-    __version__ = repo.active_branch.name + '.' + repo.git.rev_parse(repo.head.object.hexsha, short=4)
-except git.GitError:
-    pass
+except ImportError:
+    logging.debug('git import error')
+else:
+    # get git branch (if possible)
+    repo_path = os.path.abspath(os.path.dirname(__file__) + os.sep + '..')
+    try:
+        repo = git.Repo(repo_path)
+        __version__ = repo.active_branch.name + '.' + repo.git.rev_parse(repo.head.object.hexsha, short=4)
+    except git.GitError:
+        pass
 
         
