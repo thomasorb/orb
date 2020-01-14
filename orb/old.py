bug related to the calculation of the quadrants boundaries fixed

orb/old.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import time
-import math
 import traceback
 import inspect
 import re
@@ -479,12 +478,11 @@ class HDFCube(Cube):
             with orb.utils.io.open_hdf5(self.cube_path, 'r') as f:
                 if not self._silent_load:
                     progress = ProgressBar(self.quad_nb)
-
                 for iquad in range(self.quad_nb):
                     if not self._silent_load:
                         progress.update(iquad, info='Loading data')
                     x_min, x_max, y_min, y_max = self._get_quadrant_dims(
-                        iquad, self.dimx, self.dimy, int(math.sqrt(float(self.quad_nb))))
+                        iquad, self.dimx, self.dimy, int(np.sqrt(float(self.quad_nb))))
                     if slice_in_quad(x_slice, x_min, x_max) and slice_in_quad(y_slice, y_min, y_max):
                         data[max(x_min, x_slice.start) - x_slice.start:
                              min(x_max, x_slice.stop) - x_slice.start,

orb/utils/image.py
@@ -2086,17 +2086,16 @@ def get_quadrant_dims(quad_number, dimx, dimy, div_nb):
         return None
 
     index_x = quad_number % div_nb
-    index_y = (quad_number - index_x) / div_nb
-
-    x_min = int(index_x * np.ceil(dimx / div_nb))
+    index_y = (quad_number - index_x) // div_nb
+    x_min = int(index_x * np.ceil(dimx // div_nb))
     if (index_x != div_nb - 1):            
-        x_max = int((index_x  + 1) * np.ceil(dimx / div_nb))
+        x_max = int((index_x  + 1) * np.ceil(dimx // div_nb))
     else:
         x_max = dimx
 
-    y_min = int(index_y * np.ceil(dimy / div_nb))
+    y_min = int(index_y * np.ceil(dimy // div_nb))
     if (index_y != div_nb - 1):            
-        y_max = int((index_y  + 1) * np.ceil(dimy / div_nb))
+        y_max = int((index_y  + 1) * np.ceil(dimy // div_nb))
     else:
         y_max = dimy
 